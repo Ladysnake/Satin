452c2098a1b439579a8e096ce7da92cda9f5eaa7
Pyrofab <redstoneinfire@gmail.com>
2019-05-19 13:41:51 +0200

    Do Mojang's job and make uniform upload return early

--- src/main/java/ladysnake/satin/mixin/client/gl/GlUniformMixin.java
+++ src/main/java/ladysnake/satin/mixin/client/gl/GlUniformMixin.java
@@ -1,15 +1,20 @@
 package ladysnake.satin.mixin.client.gl;
 
 import net.minecraft.client.gl.GlUniform;
+import org.objectweb.asm.Opcodes;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.nio.Buffer;
 import java.nio.FloatBuffer;
 import java.nio.IntBuffer;
+
+import static org.spongepowered.asm.mixin.injection.At.Shift.AFTER;
 
 @Mixin(GlUniform.class)
 public abstract class GlUniformMixin {
@@ -19,5 +24,10 @@ public abstract class GlUniformMixin {
     private Buffer fixIntUniforms(FloatBuffer floatBuffer) {
         this.intData.clear();
         return floatBuffer;
+    }
+
+    @Inject(method = "upload", at = @At(value = "JUMP", opcode = Opcodes.IFNE, ordinal = 0, shift = AFTER), cancellable = true)
+    private void fixUploadEarlyReturn(CallbackInfo ci) {
+        ci.cancel();
     }
 }
