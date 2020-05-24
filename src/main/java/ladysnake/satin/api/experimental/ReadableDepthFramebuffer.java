f9e0454a1648a99dec9881cf3c1f0c36dbc9f59d
Pyrofab <redstoneinfire@gmail.com>
2020-05-24 11:49:47 +0200

    Add license headers

--- src/main/java/ladysnake/satin/api/experimental/ReadableDepthFramebuffer.java
+++ src/main/java/ladysnake/satin/api/experimental/ReadableDepthFramebuffer.java
@@ -1,3 +1,20 @@
+/*
+ * Satin
+ * Copyright (C) 2019-2020 Ladysnake
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; If not, see <https://www.gnu.org/licenses>.
+ */
 package ladysnake.satin.api.experimental;
 
 import ladysnake.satin.Satin;
@@ -11,7 +28,7 @@ import javax.annotation.CheckForSigned;
 import static org.apiguardian.api.API.Status.EXPERIMENTAL;
 
 /**
- * Implemented by every {@link net.minecraft.client.gl.GlFramebuffer} when the feature is enabled in the config.
+ * Implemented by every {@link net.minecraft.client.gl.Framebuffer} when the feature is enabled in the config.
  * <p>
  * This allows access to a depth texture that the framebuffer writes to instead of the usual render buffer.
  * <p>
